e1e2f92e7a76509b6a2ec1ba64f47a58c8d66ca3
Brian <wamwangimathenge@gmail.com>
2022-12-01 08:34:19 +0300

    Completed assignment

--- challenge1.js
+++ challenge1.js
@@ -1,1 +1,17 @@
-
+const grader = (marks) => {
+    if (marks > 100) {
+        return "Invalid score";
+    } else if (marks > 79) {
+        return "A"
+    } else if (marks > 59) {
+        return "B";
+    } else if (marks > 49) {
+        return "C";
+    } else if (marks > 39) {
+        return "D";
+    } else if (marks < 49) {
+        return "E";
+    } else {
+        return "Invalid score"
+    }
+}
