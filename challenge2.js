628f432ae2768d1617bdfb8fbf0d5b9f45e98834
Brian <wamwangimathenge@gmail.com>
2022-12-01 09:11:49 +0300

    Completed assignment

--- challenge2.js
+++ challenge2.js
@@ -1,1 +1,12 @@
-
+const inputSpeed = (speed) => {
+    if(speed < 70) {
+        return "OK"
+    } else {
+        const overspeed = Math.floor((speed - 70) / 5);
+        if (overspeed <= 12) {
+            return `Points: ${overspeed}`;
+        } else {
+            return `License suspended`;
+        }
+    }
+}
