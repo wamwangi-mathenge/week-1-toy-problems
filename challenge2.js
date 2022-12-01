11f9cd0ed8b8cae6a31897cd113294aebeda135b
Brian <wamwangimathenge@gmail.com>
2022-12-01 09:58:11 +0300

    Completed assignment

--- challenge2.js
+++ challenge2.js
@@ -1,12 +1,17 @@
+
+// Write a program that takes the speed of a car as input
 const inputSpeed = (speed) => {
     if(speed < 70) {
-        return "OK"
+        // If the speed is less than 70, it should print “Ok”.
+        console.log("OK")
     } else {
         const overspeed = Math.floor((speed - 70) / 5);
         if (overspeed <= 12) {
-            return `Points: ${overspeed}`;
+            // For every 5 km/s above the speed limit (70), it should give the driver one demerit point and print the total number of demerit points.
+            console.log(`Points: ${overspeed}`);
         } else {
-            return `License suspended`;
+            // If the driver gets more than 12 points, the function should print: “License suspended”.
+            console.log(`License suspended`);
         }
     }
 }
